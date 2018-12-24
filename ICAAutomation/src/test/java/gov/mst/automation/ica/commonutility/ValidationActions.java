cbeeb7e1b6ace950c367a5046d82d58b289c55cb
nanthini@mail.com.git <MSTSYS0059@MSTSYS0059>
2018-12-24 22:20:48 +0530

    push

--- ICAAutomation/src/test/java/gov/mst/automation/ica/commonutility/ValidationActions.java
+++ ICAAutomation/src/test/java/gov/mst/automation/ica/commonutility/ValidationActions.java
@@ -4,7 +4,7 @@ package gov.mst.automation.ica.commonutility;
 	* Author	 				: Nanthini PushpaRaja
 	* Created date			: Dec 08, 2018
 	* Last Edited by		: Nanthini PushpaRaja
-	* Last Edited date		: Dec 24, 2018
+	* Last Edited date		: Dec 23, 2018
 	* Description			: Class is used to define the validation actions
 */
 
