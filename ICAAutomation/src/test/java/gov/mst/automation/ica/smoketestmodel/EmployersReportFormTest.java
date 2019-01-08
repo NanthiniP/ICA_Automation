588869734103d7626b15a7f2ad3f63dd0c4111f9
nanthini@mail.com.git <MSTSYS0059@MSTSYS0059>
2019-01-08 17:46:25 +0530

    Push for Smoke and Regression suites update

--- ICAAutomation/src/test/java/gov/mst/automation/ica/smoketestmodel/EmployersReportFormTest.java
+++ ICAAutomation/src/test/java/gov/mst/automation/ica/smoketestmodel/EmployersReportFormTest.java
@@ -4,7 +4,7 @@ package gov.mst.automation.ica.smoketestmodel;
  	* Author	 				: Nanthini PushpaRaja
  	* Created date			: Nov 26, 2018
  	* Last Edited by		: Nanthini PushpaRaja
- 	* Last Edited date		: Dec 08, 2018
+ 	* Last Edited date		: Jan 05, 2018
  	* Description			: Class is used to execute the action in Employer Report Form
 */ 
 
